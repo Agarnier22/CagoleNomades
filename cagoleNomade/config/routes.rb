596b37665fcb9e83821a1c3c01eb9d297e51ca07
rachedi omar <omartchik@gmail.com>
2021-11-10 00:32:43 +0100

    see more btn done + about page

--- cagoleNomade/config/routes.rb
+++ cagoleNomade/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'about', :to => "infos#about"
+  get 'infos/contact'
+  get 'infos/pannier'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pages
+  root to: "pages#index"
 end
